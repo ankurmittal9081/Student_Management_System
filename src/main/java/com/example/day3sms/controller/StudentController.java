d607173d963fc1d8f18efdaad700016097479930
Ankur Mittal <ankurmittal9081@gmail.com>
2026-01-20 14:48:55 +0530

    make a StudentService class

--- src/main/java/com/example/day3sms/controller/StudentController.java
+++ src/main/java/com/example/day3sms/controller/StudentController.java
@@ -1,22 +1,27 @@
 package com.example.day3sms.controller;
 
 
+import com.example.day3sms.model.StudentModel;
+import com.example.day3sms.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class StudentController {
+public class StudentController
+{
+    //controller -> request //Service -> Logic // Repository -> database // Model -> data Schema
+    private final StudentService service;
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
 
-
-    //controller -> request
-    //Service -> Logic
-    // Repository -> database
-    // Model -> data Schema
-
-    @GetMapping("/")
-    public String hello(){
-        return "Homepage for student";
+    //create function API
+    @PostMapping("add-student/")
+    public StudentModel addStudent(@RequestBody StudentModel student){
+        return service.addStudent(student);
     }
 
 }
