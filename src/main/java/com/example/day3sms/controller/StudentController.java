b6811a9818f3308810e95e9c2321b1fea4c2b40c
Ankur Mittal <ankurmittal9081@gmail.com>
2026-02-02 14:12:18 +0530

    make a StudentService class

--- src/main/java/com/example/day3sms/controller/StudentController.java
+++ src/main/java/com/example/day3sms/controller/StudentController.java
@@ -3,23 +3,38 @@ package com.example.day3sms.controller;
 
 import com.example.day3sms.model.StudentModel;
 import com.example.day3sms.service.StudentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+public class StudentController {
 
-public class StudentController
-{
-    //controller -> request //Service -> Logic // Repository -> database // Model -> data Schema
     private final StudentService service;
+
     public StudentController(StudentService service) {
         this.service = service;
     }
 
-    //create function API
-    @PostMapping("add-student/")
+    @PostMapping("/add-student")
     public StudentModel addStudent(@RequestBody StudentModel student){
         return service.addStudent(student);
+    }
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudents(){
+        return service.getStudents();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable String id){
+        service.deleteStudent(id);
+        return "Student Deleted Successfully";
+    }
+
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id,
+                                      @RequestBody StudentModel student){
+        return service.updateStudent(id, student);
     }
 }
--- src/main/java/com/example/day3sms/service/StudentService.java
+++ src/main/java/com/example/day3sms/service/StudentService.java
@@ -3,19 +3,41 @@ package com.example.day3sms.service;
 import com.example.day3sms.model.StudentModel;
 import com.example.day3sms.repository.StudentRepositiory;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class StudentService {
-    private final StudentRepositiory repositiory;
 
-    //create
+    private final StudentRepositiory repository;
 
-    public StudentService(StudentRepositiory repositiory) {
-        this.repositiory = repositiory;
+    public StudentService(StudentRepositiory repository) {
+        this.repository = repository;
     }
 
+    // create
     public StudentModel addStudent(StudentModel student){
-        return repositiory.save(student);
+        return repository.save(student);
     }
 
+    // display
+    public List<StudentModel> getStudents(){
+        return repository.findAll();
+    }
+
+    // delete
+    public void deleteStudent(String id){
+        repository.deleteById(id);
+    }
+
+    // update
+    public StudentModel updateStudent(String id, StudentModel student){
+        StudentModel existingStudent = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No Student found"));
+
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+
+        return repository.save(existingStudent);
+    }
 }
