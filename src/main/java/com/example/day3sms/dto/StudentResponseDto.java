0183137cb448c8a06ead5b2df7bc539df829bba1
Ankur Mittal <ankurmittal9081@gmail.com>
2026-02-03 12:15:20 +0530

    make a StudentService class

---
+++ src/main/java/com/example/day3sms/dto/StudentResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.day3sms.dto;
+
+public record StudentResponseDto(
+        String id,
+     String name,
+     String email,
+     int age
+)
+
+{
+
+}
---
+++ src/main/java/com/example/day3sms/exception/StudentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.day3sms.exception;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
+
+/*
+
+ */
